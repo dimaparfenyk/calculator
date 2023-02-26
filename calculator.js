b89aaf8d6ce9da1da57ed2b0fd915b87dbf1dbec
Dmitrij <92674961+dimaparfenyk@users.noreply.github.com>
2023-02-26 15:40:04 +0300

    update

--- calculator.js
+++ calculator.js
@@ -1,62 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-
-//     const carPrice = document.querySelector('.car-price').value; //Загальна ціна
-//     const startPaymentProcent = document.querySelector('.start-payment-procent'); // Перший внесок в процентах від 30 до 70
-//     const startPaymentInput = document.querySelector('.procent-input'); // Інпут де показаний процент
-//     const amountCredit = document.querySelector('.amount-credit'); // Сума кредиту грн
-//     const creditTerm = document.querySelector('.js-credit-term'); // срок кредиту місяці
-//     const creditTermInput = document.querySelector('.js-credit-term-input'); // Срок кредиту текст для інформативності
-//     const calcResult = document.getElementById("js-result"); // Результат
-
-//     let creditProcent = '';
-
-//     creditTermInput.value = creditTerm.value;
-
-
-
-
-//     function calcCreditProcent() {
-//         if (creditTerm.value >= 36 && creditTerm.value <= 60 && startPaymentProcent.value == 30 ) {
-//             creditProcent = 12.99;
-//             console.log(creditProcent + '%');
-//         }
-//         if (creditTerm.value >= 36 && creditTerm.value <= 60 && startPaymentProcent.value == 70 ) {
-//             creditProcent = 0.01;
-//             console.log(creditProcent + '%');
-//         }
-//     };
-//     calcCreditProcent()
-
-//     // Розрахунок суми кредиту
-//     function amountCreditCalc() {
-//         amountCredit.value = carPrice - (carPrice / 100 * startPaymentProcent.value);
-//     };
-
-//     // Розрахунок місячного платежу
-//     function resultCalculation() {
-//         calcResult.innerHTML = Math.round((amountCredit.value / 12) + ((amountCredit.value / 12) / 100 * 6 ));
-//     }
-
-//     amountCreditCalc();
-//     resultCalculation();
-
-//     // Розрахунок при виборі проценту початкового внеску
-//     startPaymentProcent.oninput = function() {
-//         startPaymentInput.value = this.value + '%';
-//         amountCreditCalc();
-//         resultCalculation();
-//         calcCreditProcent();
-//     };
-
-//     // При зміні терміну кредитування
-//     creditTerm.oninput = function() {
-//         creditTermInput.value = this.value;
-//         calcCreditProcent()
-//     };
-// });
-
-let creditRate=12.99;
-
     const carPrice=document.querySelector('.car-price').value;
     const procentSlider=document.querySelector('.start-payment-procent');
     const procentPrepaymentValue=document.querySelector('.procent-input');
@@ -64,17 +5,16 @@ let creditRate=12.99;
     const creditMonthesSlider=document.querySelector('.js-credit-term');
     const creditAmount=document.querySelector('.amount-credit');
     const creditPerMonth=document.querySelector('.calc-result');
-
+    
+    let creditRate=12.99;
     // инициализация суммы кредита и месячной суммы при первой загрузке
     calculateCreditAmount();
     calculateCreditPerMonth(creditRate);  
 
     // присвоение значения процентной ставки в зависимости от времени и первичного взноса 
 function assignCreditRateValue(){
-
     const procent=Number(procentPrepaymentValue.value);
     const monthCount=Number(creditMonthesSlider.value);
-    console.log( monthCount)
 
     switch (procent) {
         case 30:
@@ -95,7 +35,6 @@ function assignCreditRateValue(){
                 console.log(creditRate);
             }
             break;
-
             case 40:
                 if(monthCount>=12&&monthCount<24){
                     creditRate=5.99;
@@ -180,7 +119,6 @@ function assignCreditRateValue(){
             break;
     };
 };
-
 
 //  обработка событий при скроле ползунков-инпутов
 procentSlider.addEventListener('input', onProcentSliderChange);
